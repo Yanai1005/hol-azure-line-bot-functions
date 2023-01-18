67b62570173f61a4e103eda8cec73c8839e5b04a
Yanai1005 <tatsuya871_1005@icloud.com>
2023-01-18 18:48:58 +0900

    Update reply message

--- Functions/Webhook.cs
+++ Functions/Webhook.cs
@@ -60,7 +60,7 @@ namespace Functions
             // この一行をコメントアウトする
             await Reply(firstEvent.ReplyToken, firstEvent.Message.Text);
 
-            以下3行のコメントアウトをはずす
+            //以下3行のコメントアウトをはずす
             var sendMessage = firstEvent.Message.Text;
             var replyText = sendMessage == "今日の天気は？" ? "晴れです。" : sendMessage;
             await Reply(firstEvent.ReplyToken, replyText);
